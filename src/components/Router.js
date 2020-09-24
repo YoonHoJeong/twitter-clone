0eba5c1254b5503cb8828ac76998edb5e9915619
Jeong YoonHo <uno8941@gmail.com>
2020-09-24 11:55:26 +0900

    #2.6 logout, useHistory hook

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 export default ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         <Route exact path="/">
           {isLoggedIn ? <Home /> : <Auth />}
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
         </Route>
       </Switch>
     </Router>
