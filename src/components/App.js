dec671f83208c6e96b6c0c35c020fc16451bd2f9
Jeong YoonHo <uno8941@gmail.com>
2020-09-24 11:49:17 +0900

    #2.5 social login - google, github

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import Router from "./Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-
-  return (
-    <>
-      <Router isLoggedIn={isLoggedIn} />
-    </>
-  );
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    // tracking auth state
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return <>{init ? <Router isLoggedIn={isLoggedIn} /> : "Initializing..."}</>;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,16 +1,38 @@
+import { firebaseInstance, authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const setState = {
     email: setEmail,
     password: setPassword,
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    try {
+      if (newAccount) {
+        // create Account
+        const data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        console.log(data);
+      } else {
+        // login
+        const data = await authService.signInWithEmailAndPassword(
+          email,
+          password
+        );
+        console.log(data);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
   };
   const onChange = (e) => {
     const {
@@ -20,6 +42,30 @@ const Auth = () => {
 
     setFunction(value);
   };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  const onSocialClick = async (e) => {
+    let provider;
+    const {
+      target: { name: providerName },
+    } = e;
+
+    const onSocials = {
+      github: () => {
+        provider = new firebaseInstance.auth.GithubAuthProvider();
+      },
+      google: () => {
+        provider = new firebaseInstance.auth.GoogleAuthProvider();
+      },
+    };
+
+    onSocials[providerName]();
+
+    const data = await firebaseInstance.auth().signInWithPopup(provider);
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -39,11 +85,23 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
+
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
       </div>
     </div>
   );
